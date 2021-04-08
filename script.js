a0a1ad964e3ee360700c2dd26429cdd030a05748
Rida <mrida@live.unc.edu>
2021-04-08 14:28:15 -0400

    correction

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let updateInnerOf = function (loadElement, value) {
 
 
 
-const string = "53 When they had crossed over, they came to shore at Gennesaret and anchored there. 54 As they got out of the boat, people immediately recognized him. 55 They hurried throughout that region and began to carry the sick on mats to wherever they heard he was. 56 Wherever he went, into villages, towns, or the country, they laid the sick in the marketplaces and begged him that they might touch just the end of his robe. And everyone who touched it was healed.";
+const string = "45 Immediately he made his disciples get into the boat and go ahead of him to the other side, to Bethsaida, while he dismissed the crowd. 46 After he said good-bye to them, he went away to the mountain to pray. 47 Well into the night, the boat was in the middle of the sea, and he was alone on the land. 48 He saw them straining at the oars, because the wind was against them. Very early in the morning he came toward them walking on the sea and wanted to pass by them. 49 When they saw him walking on the sea, they thought it was a ghost and cried out, 50 because they all saw him and were terrified. Immediately he spoke with them and said, “Have courage! It is I. Don’t be afraid.” 51 Then he got into the boat with them, and the wind ceased. They were completely astounded, 52 because they had not understood about the loaves. Instead, their hearts were hardened.";
 
-window.addEventListener("load", alert(numbersToSuperScript(string)));
+window.addEventListener("load", console.log(numbersToSuperScript(string)));
 
